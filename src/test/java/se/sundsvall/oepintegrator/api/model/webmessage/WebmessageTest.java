3c1d3325ce6d6b0a43fe55daead451204d3399cf
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-03-25 16:01:01 +0100

    UF-14928: Build issue

--- src/test/java/se/sundsvall/oepintegrator/api/model/webmessage/WebmessageTest.java
+++ src/test/java/se/sundsvall/oepintegrator/api/model/webmessage/WebmessageTest.java
@@ -5,15 +5,23 @@ import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static com.google.code.beanmatchers.BeanMatchers.registerValueGenerator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.allOf;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Random;
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class WebmessageTest {
+
+	@BeforeAll
+	static void setup() {
+		registerValueGenerator(() -> LocalDateTime.now().plusDays(new Random().nextInt()), LocalDateTime.class);
+	}
 
 	@Test
 	void testBean() {
